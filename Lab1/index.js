4e88d826ef3ae48a355c085ec9d8d9b96b310bcf
McDonaghMichael <G00421944@atu.ie>
2024-09-26 17:49:47 +0100

    Wrote a function that multiply each number under 70 by 2

--- Lab1/index.js
+++ Lab1/index.js
@@ -2,6 +2,16 @@ const getData = (variable) => variable;
 
 const getSum = (num1, num2) => num1 + num2;
 
-console.log(getSum(1,4));
+const ages = [25, 31, 42, 77];
 
+const calculateAges = ages.map((age) => {
+    if(age < 70){
+       return age * 2;
+    }
+    return age;
+});
+
+console.log(calculateAges);
+
+console.log(getSum(1,4));
 console.log(getData("Hello"));

3bd1c67b26ed9b38b5d1fb9f7513f65915463fe9
McDonaghMichael <G00421944@atu.ie>
2024-09-26 17:54:05 +0100

    Created an addTask function

--- Lab1/index.js
+++ Lab1/index.js
@@ -19,4 +19,14 @@ console.log(getData("Hello"));
 
 // QUESTION 3
 
+// A
 var items = ["bread", "milk", "butter"];
+
+// B
+const addTask = (task) => {
+    items.push(task);
+    console.log(task + " has been added to the array");
+    return items.length;
+}
+
+console.log(addTask("cheese"));

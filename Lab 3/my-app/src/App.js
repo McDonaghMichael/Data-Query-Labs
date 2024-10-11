23e5d906682291193caa654986307e4e04fa8cd2
McDonaghMichael <G00421944@atu.ie>
2024-10-11 13:34:18 +0000

    Created Create.js component

--- Lab 3/my-app/src/App.js
+++ Lab 3/my-app/src/App.js
@@ -3,10 +3,12 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import Content from './components/Content';
+import NavigationBar from './components/NavigationBar';
 
 function App() {
     return (
         <Router>
+            <NavigationBar />
             <Routes>
                 <Route path="/home" element={<Content />} />
                 <Route path="/read" element={<Footer />} />

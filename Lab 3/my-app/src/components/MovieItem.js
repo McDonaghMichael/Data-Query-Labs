fd389be03163144bcee0d208b46d94f8f8cfa2e4
McDonaghMichael <G00421944@atu.ie>
2024-10-11 14:03:28 +0000

    MovieItem now accurately displays movie information

--- Lab 3/my-app/src/components/MovieItem.js
+++ Lab 3/my-app/src/components/MovieItem.js
@@ -1,8 +1,11 @@
-import React from 'react';
-
-
 const MovieItem = (props) => {
-    return console.log(props.data);
+    return (
+    <div>
+        <h4>
+            {props.data.Title}
+        </h4>
+        <p>Year: {props.data.Year}</p>
+    </div>);
 }
 
 export default MovieItem;
--- Lab 3/my-app/src/components/Movies.js
+++ Lab 3/my-app/src/components/Movies.js
@@ -2,9 +2,9 @@ import React from 'react';
 import MovieItem from './MovieItem';
 
 const Movies = (props) => {
-    return props.movieData.forEach(function(movie) {
-                <MovieItem data={movie}/>
-            });
+    return props.movieData.map((movie) => (
+        <MovieItem data={movie} key={movie.imdbID} />
+      ));
 }
 
 export default Movies;

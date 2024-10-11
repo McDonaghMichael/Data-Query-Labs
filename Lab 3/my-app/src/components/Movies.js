2119146a4e92ae0a43f3afd7983a50b0ba47d2a3
McDonaghMichael <G00421944@atu.ie>
2024-10-11 13:47:06 +0000

    Ammended movie data to the read.js file

---
+++ Lab 3/my-app/src/components/Movies.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const Movies = () => {
+    return (
+        <div>
+            <h3>
+                Hello from movies component
+            </h3>
+        </div>
+    );
+}
+
+export default Movies;

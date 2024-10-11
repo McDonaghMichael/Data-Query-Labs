dae741cc7d36440f1fbc4de0616473527f6b41df
McDonaghMichael <G00421944@atu.ie>
2024-10-11 13:35:08 +0000

    Created Read.js component

---
+++ Lab 3/my-app/src/components/Read.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Read = () => {
+    return (
+        <div>
+            <h3>
+                Hello from read component
+            </h3>
+        </div>
+    );
+}
+
+export default Read;

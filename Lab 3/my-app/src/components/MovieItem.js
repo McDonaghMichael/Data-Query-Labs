575860cb1fae14944a748ad85629d1be64d2992c
McDonaghMichael <G00421944@atu.ie>
2024-10-11 14:28:52 +0000

    Movies are now listed in boostrap cards

--- Lab 3/my-app/src/components/MovieItem.js
+++ Lab 3/my-app/src/components/MovieItem.js
@@ -1,11 +1,19 @@
+import Card from 'react-bootstrap/Card';
+import { useEffect } from "react";
+
+
 const MovieItem = (props) => {
     return (
     <div>
-        <h4>
-            {props.data.Title}
-        </h4>
-        <p>Year: {props.data.Year}</p>
-        <img src={props.data.Poster}></img>
+        <Card>
+        <Card.Header>{props.data.Title}</Card.Header>
+        <Card.Body>
+          <blockquote className="blockquote mb-0">
+            <img src={props.data.Poster} alt={props.data.Title} />
+            <footer>{props.data.Year}</footer>
+          </blockquote>
+        </Card.Body>
+      </Card>
     </div>);
 }
 

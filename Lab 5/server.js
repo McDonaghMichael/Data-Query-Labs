ffebc281deaa80c6f0b45e31e635ffd4ce78fb4f
McDonaghMichael <G00421944@atu.ie>
2024-10-25 13:26:47 +0000

    /index route has been implemented and loads in our inex.html file into the browser

--- Lab 5/server.js
+++ Lab 5/server.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
     res.send('Welcome to Data Respresentation & Querying');
+});
+
+
+app.get('/index', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(port, () => {

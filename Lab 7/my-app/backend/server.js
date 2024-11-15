06ed43388aa649ec870e283dd4ef70df98a6c08a
McDonaghMichael <G00421944@atu.ie>
2024-11-15 10:34:08 +0000

    Initial Lab 7

---
+++ Lab 7/my-app/backend/server.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+const port = 4000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.send('Server');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
+
+app.use(cors());
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+const movies = [
+    {
+      "Title": "Avengers: Infinity War (server)",
+      "Year": "2018",
+      "imdbID": "tt4154756",
+      "Type": "movie",
+      "Poster": "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg"
+    },
+    {
+      "Title": "Captain America: Civil War (server)",
+      "Year": "2016",
+      "imdbID": "tt3498820",
+      "Type": "movie",
+      "Poster": "https://m.media-amazon.com/images/M/MV5BMjQ0MTgyNjAxMV5BMl5BanBnXkFtZTgwNjUzMDkyODE@._V1_SX300.jpg"
+    },
+    {
+      "Title": "World War Z (server)",
+      "Year": "2013",
+      "imdbID": "tt0816711",
+      "Type": "movie",
+      "Poster": "https://m.media-amazon.com/images/M/MV5BNDQ4YzFmNzktMmM5ZC00MDZjLTk1OTktNDE2ODE4YjM2MjJjXkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_SX300.jpg"
+    }
+];
+
+app.post('/api/movies', (req, res) => {
+    movies.push({
+        "Title": req.body.title,
+        "Year": req.body.year,
+        "imdbID": "tt24323",
+        "Type": "movie",
+        "Poster": req.body.poster,
+    })
+    res.status(201).json(req.body);
+
+});
+
+app.get('/api/movies', (req, res) => {
+    res.status(201).json({ movies: movies });
+   
+});

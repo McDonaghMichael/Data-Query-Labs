8b129142d81c1a4f1580d3bc309e4ef74b6d30e3
McDonaghMichael <G00421944@atu.ie>
2024-11-29 14:17:56 +0000

    Added delete button

--- Lab 9/my-app/src/components/MovieItem.js
+++ Lab 9/my-app/src/components/MovieItem.js
@@ -1,11 +1,23 @@
 import Card from 'react-bootstrap/Card';
 import { Link } from 'react-router-dom';
-
+import Button from 'react-bootstrap/Button';
+import axios from 'axios';
 /**
  * MovieItem will display the title, year and poster of a movie
  * and it will be displayed using the Boostrap Card
  */
 const MovieItem = (props) => {
+
+  const handleDelete = (e) => {
+    e.preventDefault();
+      axios.delete('http://localhost:4000/api/movie/' + props.myMovie._id)
+          .then(() => {
+              props.Reload(); // Refresh the movie list after deletion
+          })
+          .catch((error) => {
+              console.error("Error deleting movie:", error);
+          });
+      };
 
     // MovieItem component and its data is returned in the form of a boostrap card
     return (
@@ -17,6 +29,7 @@ const MovieItem = (props) => {
             <img src={props.data.poster} alt={props.data.title} />
             <footer>{props.data.year}</footer>
             <Link to={"/edit/" + props.data._id} className="btn btn-primary">Edit</Link>
+            <Button variant="danger" onClick={handleDelete}>Delete</Button>
           </blockquote>
         </Card.Body>
       </Card>
